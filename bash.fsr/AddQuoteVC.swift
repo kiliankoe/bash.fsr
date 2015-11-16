37aaebbd062c2a2fee69ac16548b4c64c7784524
Kilian Koeltzsch <me@kilian.io>
2015-11-16 16:52:09 +0100

    success ux and don't submit empty quotes

--- bash.fsr/AddQuoteVC.swift
+++ bash.fsr/AddQuoteVC.swift
@@ -23,8 +23,19 @@ class AddQuoteVC: UIViewController {
 	
 	func addQuote() {
 		let quote = textView?.text
+		guard quote != "" else {
+			let alert = UIAlertController(title: "Nop", message: "Quote is empty! Try again...", preferredStyle: UIAlertControllerStyle.Alert)
+			alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil))
+			self.presentViewController(alert, animated: true, completion: nil)
+			return
+		}
 		Bash.addQuote(quote!) { [unowned self] (success) -> Void in
-			if !success {
+			if success {
+				self.navigationController?.popViewControllerAnimated(true)
+				let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
+				let mainVC = appDelegate?.window?.rootViewController?.childViewControllers[0] as? BashViewController
+				mainVC?.updateQuotes()
+			} else {
 				let alert = UIAlertController(title: "Nop", message: "Submit failed!", preferredStyle: UIAlertControllerStyle.Alert)
 				alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil))
 				self.presentViewController(alert, animated: true, completion: nil)
