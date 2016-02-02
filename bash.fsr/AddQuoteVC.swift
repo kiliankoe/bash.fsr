e04ff9b87bbd48cdc95872896a56e832e0d667c8
h4llow3En <Doering.Felix@googlemail.com>
2016-02-02 16:33:28 +0100

    add UIActivityIndicatorView

--- bash.fsr/AddQuoteVC.swift
+++ bash.fsr/AddQuoteVC.swift
@@ -15,22 +15,29 @@ class AddQuoteVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 		
-		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: "addQuote")
+        let activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 20, 20))
+        activityIndicator.color = UIColor.blackColor()
+        activityIndicator.startAnimating()
+        activityButton = UIBarButtonItem(customView: activityIndicator)
+        doneButton = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: "addQuote")
+        
+        navigationItem.rightBarButtonItem = doneButton
 
 		automaticallyAdjustsScrollViewInsets = false
         textView?.becomeFirstResponder()
     }
 	
 	func addQuote() {
-        self.navigationItem.rightBarButtonItem!.action = nil
 		let quote = textView?.text
 		guard quote != "" else {
 			let alert = UIAlertController(title: "Nop", message: "Quote is empty! Try again...", preferredStyle: UIAlertControllerStyle.Alert)
 			alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil))
 			self.presentViewController(alert, animated: true, completion: nil)
-            self.navigationItem.rightBarButtonItem?.action = Selector("addQuote")
 			return
 		}
+
+        self.navigationItem.rightBarButtonItem = self.activityButton
+
 		Bash.addQuote(quote!) { [unowned self] (success) -> Void in
 			if success {
 				self.navigationController?.popViewControllerAnimated(true)
@@ -41,9 +48,9 @@ class AddQuoteVC: UIViewController {
 				let alert = UIAlertController(title: "Nop", message: "Submit failed!", preferredStyle: UIAlertControllerStyle.Alert)
 				alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil))
 				self.presentViewController(alert, animated: true, completion: nil)
+                self.navigationItem.rightBarButtonItem = self.doneButton
 			}
 		}
-        self.navigationItem.rightBarButtonItem?.action = Selector("addQuote")
 	}
 
 }
