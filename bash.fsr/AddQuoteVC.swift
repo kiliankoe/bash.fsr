b7a4997ea69418357f4aea20e4dbe79c624c27ac
Kilian Koeltzsch <me@kilian.io>
2017-04-05 06:30:48 +0200

    explicit and weak self

--- bash.fsr/AddQuoteVC.swift
+++ bash.fsr/AddQuoteVC.swift
@@ -13,7 +13,7 @@ class AddQuoteVC: UIViewController {
 	@IBOutlet weak var textView: UITextView?
 	
 	var activityButton: UIBarButtonItem?
-    	var doneButton: UIBarButtonItem?
+    var doneButton: UIBarButtonItem?
 	
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,37 +21,39 @@ class AddQuoteVC: UIViewController {
         let activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
         activityIndicator.color = UIColor.black
         activityIndicator.startAnimating()
-        activityButton = UIBarButtonItem(customView: activityIndicator)
-        doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(AddQuoteVC.addQuote))
+        self.activityButton = UIBarButtonItem(customView: activityIndicator)
+        self.doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(AddQuoteVC.addQuote))
         
-        navigationItem.rightBarButtonItem = doneButton
+        self.navigationItem.rightBarButtonItem = doneButton
 
-		automaticallyAdjustsScrollViewInsets = false
-        textView?.becomeFirstResponder()
+		self.automaticallyAdjustsScrollViewInsets = false
+        self.textView?.becomeFirstResponder()
     }
 	
 	func addQuote() {
-		let quote = textView?.text
-		guard quote != "" else {
+		guard
+            let quote = self.textView?.text,
+            quote != ""
+        else {
 			let alert = UIAlertController(title: "Nop", message: "Quote is empty! Try again...", preferredStyle: UIAlertControllerStyle.alert)
-			alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil))
+			alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.cancel, handler: nil))
 			self.present(alert, animated: true, completion: nil)
 			return
 		}
 
         self.navigationItem.rightBarButtonItem = self.activityButton
 
-		Bash.addQuote(quote!) { [unowned self] (success) -> Void in
+		Bash.addQuote(quote) { [weak self] (success) -> Void in
 			if success {
-				self.navigationController?.popViewController(animated: true)
+				self?.navigationController?.popViewController(animated: true)
 				let appDelegate = UIApplication.shared.delegate as? AppDelegate
 				let mainVC = appDelegate?.window?.rootViewController?.childViewControllers[0] as? BashViewController
 				mainVC?.updateQuotes()
 			} else {
 				let alert = UIAlertController(title: "Nop", message: "Submit failed!", preferredStyle: UIAlertControllerStyle.alert)
-				alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil))
-				self.present(alert, animated: true, completion: nil)
-                self.navigationItem.rightBarButtonItem = self.doneButton
+				alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.cancel, handler: nil))
+				self?.present(alert, animated: true, completion: nil)
+                self?.navigationItem.rightBarButtonItem = self?.doneButton
 			}
 		}
 	}
