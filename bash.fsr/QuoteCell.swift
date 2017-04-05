3329c5ad3f897e8cf55b5abbf31df30ec5b69020
Kilian Koeltzsch <me@kilian.io>
2017-04-05 06:35:26 +0200

    remove unecessary overrides

--- bash.fsr/QuoteCell.swift
+++ bash.fsr/QuoteCell.swift
@@ -39,16 +39,4 @@ class QuoteCell: UITableViewCell {
 	@IBOutlet weak var idLabel: UILabel?
 	@IBOutlet weak var ratingLabel: UILabel?
 	@IBOutlet weak var quoteLabel: UILabel?
-	
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
